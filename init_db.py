5031ec0c1d32789711026938ecda538daec21a58
Douglas Byers <douglasbyers@live.ca>
2023-03-22 14:37:25 -0400

    Profile Creation

--- init_db.py
+++ init_db.py
@@ -12,9 +12,9 @@ cur.execute("INSERT INTO login (username, password, isprofilecreated) VALUES (?,
             ('Doug', 'dougpass', 0)
             )
 
-cur.execute("INSERT INTO profiles (username, height, weight, sex, age) VALUES (?, ?, ?, ?, ?)",
-            ('Doug', 72.0, 150.0, 'male', 22)
-            )
+#cur.execute("INSERT INTO profiles (username, height, weight, sex, age) VALUES (?, ?, ?, ?, ?)",
+#            ('Doug', 72.0, 150.0, 'male', 22)
+#            )
 
 cur.execute("INSERT INTO workouts (username, filename, workout, elapsedtime, intensity, calories) VALUES (?, ?, ?, ?, ?, ?)",
             ('Doug', 'test.mp4', 'deadlift', 42.0, 2, 100.0)
