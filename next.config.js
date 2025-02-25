35b1512b9b81a83e7b9bf46747b8d7744f88047b
Niko Hoogeveen <niko.hoogeveen@gmail.com>
2025-02-24 23:24:43 -0500

    Added next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+output: 'export',
+basePath: '/next-site',
+images: {
+    unoptimized: true,
+},
+}
+
+module.exports = nextConfig
+
